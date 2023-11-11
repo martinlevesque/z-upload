7d1c34695dd8a6d225c1b7bf7b4d02c3a5d2fbaf
Martin Levesque <levesque.martin@gmail.com>
2023-11-11 11:41:24 -0500

    wip

---
+++ tests/end_to_end.sh
@@ -0,0 +1,22 @@
+
+set -e
+
+make build
+
+
+make server-sample &
+sleep 3
+
+rm -f /tmp/testfile-out.txt
+echo "testcontentfile!" > ./tmp/testfile.txt
+
+./zig-out/bin/z-upload -client ./tmp/testfile.txt /tmp/testfile-out.txt@127.0.0.1:8000
+
+expected_checksum=$(cat ./tmp/testfile.txt | cksum)
+
+given_checksum=$(cat /tmp/testfile-out.txt | cksum)
+
+if [ "$expected_checksum" != "$given_checksum" ]; then
+    echo "Checksums do not match!"
+    exit 1
+fi
